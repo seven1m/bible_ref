192f7c6282973e958e50c2f27c944579e6bc7b0b
Tim Morgan <tim@timmorgan.org>
2014-07-06 18:02:05 -0500

    Normalize verse ranges.

--- lib/bible_api/reference.rb
+++ lib/bible_api/reference.rb
@@ -1,36 +1,60 @@
 require_relative './book'
+require_relative './parser'
 
 module BibleApi
   class Reference
 
+    attr_reader :book
     attr_reader :reference
 
     def initialize(reference)
       @reference = reference
-      (book_name, numbers) = split_book
-      @book = Book.new(book_name).id
-      (first_verse, last_verse) = split_numbers(numbers)
+      @details = parse
     end
 
-    def range
-      [
-        { book: @book, chapter: @chapter, verse: @verse },
-        { book: @book, chapter: @chapter, verse: @verse }
-      ]
+    def ranges
+      return nil unless valid?
+      @chapter = nil
+      [@details[:refs]].flatten.map do |ref|
+        normalize_range(ref) || normalize_ref(ref)
+      end
+    end
+
+    def book
+      @book ||= Book.new(@details[:book]).id
+    end
+
+    def valid?
+      @details and book
     end
 
     private
 
-    def split_book
-      # TODO smart regex here
-      @reference.split
+    def parse
+      begin
+        parsed = Parser.new.parse(@reference)
+      rescue Parslet::ParseFailed
+        nil
+      else
+        ParserTransform.new.apply(parsed)
+      end
     end
 
-    def split_numbers(numbers)
-      chapter, verse = numbers.split(':', 2)
-      (first_verse, last_verse) = verse.split('-')
-      @chapter = chapter.to_i
-      @verse = first_verse.to_i
+    def normalize_range(ref)
+      return unless range = ref[:range]
+      ch = range.detect { |_, r| r[:chapter] }
+      @chapter = ch.last[:chapter] if ch
+      [
+        { book: @book, chapter: @chapter }.merge(range[:from]),
+        { book: @book, chapter: @chapter }.merge(range[:to])
+      ]
+    end
+
+    def normalize_ref(ref)
+      @chapter = ref[:chapter] if ref[:chapter]
+      (1..2).map do
+        { book: @book, chapter: @chapter }.merge(ref)
+      end
     end
 
   end
--- spec/bible_api/reference_spec.rb
+++ spec/bible_api/reference_spec.rb
@@ -10,25 +10,68 @@ describe BibleApi::Reference do
     end
   end
 
-  describe '#range' do
+  describe '#valid?' do
+    context 'given a known reference' do
+      subject { BibleApi::Reference.new('John 3:16') }
+
+      it 'is valid' do
+        expect(subject).to be_valid
+      end
+    end
+
+    context 'given an unknown book' do
+      subject { BibleApi::Reference.new('Bad 4:1') }
+
+      it 'is not valid' do
+        expect(subject).to_not be_valid
+      end
+    end
+
+    context 'given an poorly-formatted reference' do
+      subject { BibleApi::Reference.new('foo12x34') }
+
+      it 'is not valid' do
+        expect(subject).to_not be_valid
+      end
+    end
+  end
+
+  describe '#ranges' do
     context 'given a single verse' do
       subject { BibleApi::Reference.new('John 3:16') }
 
-      it 'returns an array with the same verse twice' do
-        expect(subject.range).to eq([
-          { book: 'JHN', chapter: 3, verse: 16 },
-          { book: 'JHN', chapter: 3, verse: 16 }
+      it 'returns a range with the same verse twice' do
+        expect(subject.ranges).to eq([
+          [{ book: 'JHN', chapter: 3, verse: 16 },
+           { book: 'JHN', chapter: 3, verse: 16 }]
         ])
       end
     end
 
-    context 'given a range of verses in a single chapter' do
+    context 'given a ranges of verses' do
       subject { BibleApi::Reference.new('1 John 5:11-12') }
 
-      it 'returns an array with the same verse twice' do
-        expect(subject.range).to eq([
-          { book: '1JN', chapter: 5, verse: 11 },
-          { book: '1JN', chapter: 5, verse: 12 }
+      it 'returns a single range with first and last verse' do
+        expect(subject.ranges).to eq([
+          [{ book: '1JN', chapter: 5, verse: 11 },
+           { book: '1JN', chapter: 5, verse: 12 }]
+        ])
+      end
+    end
+
+    context 'given a complicated multi-chapter, multi-verse reference' do
+      subject { BibleApi::Reference.new('Romans 12:1,3-4 & 13:2-4,7-8') }
+
+      it 'returns all rangess in an array' do
+        expect(subject.ranges).to eq([
+          [{ book: 'ROM', chapter: 12, verse: 1 },
+           { book: 'ROM', chapter: 12, verse: 1 }],
+          [{ book: 'ROM', chapter: 12, verse: 3 },
+           { book: 'ROM', chapter: 12, verse: 4 }],
+          [{ book: 'ROM', chapter: 13, verse: 2 },
+           { book: 'ROM', chapter: 13, verse: 4 }],
+          [{ book: 'ROM', chapter: 13, verse: 7 },
+           { book: 'ROM', chapter: 13, verse: 8 }]
         ])
       end
     end
