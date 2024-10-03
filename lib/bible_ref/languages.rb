1dc000f749683036ed90bdc889ef03c928174665
Paul Artman <zelekeit@gmail.com>
2024-10-03 15:37:00 +0300

    Add books in Russian language.

--- lib/bible_ref/languages.rb
+++ lib/bible_ref/languages.rb
@@ -1,17 +1,19 @@
 require_relative 'languages/cherokee'
+require_relative 'languages/czech'
 require_relative 'languages/english'
 require_relative 'languages/latin'
 require_relative 'languages/portuguese'
 require_relative 'languages/romanian'
-require_relative 'languages/czech'
+require_relative 'languages/russian'
 
 module BibleRef
   LANGUAGES = {
     'chr' => Languages::Cherokee,
+    'cze' => Languages::Czech,
     'eng' => Languages::English,
     'lat' => Languages::Latin,
     'por' => Languages::Portuguese,
     'ron' => Languages::Romanian,
-    'cze' => Languages::Czech
+    'rus' => Languages::Russian
   }.freeze
 end
